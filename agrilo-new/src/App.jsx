e4226459fed22ab3890eab9e406cb5f5726f7652
Joseph <olorijoseph2002@gmail.com>
2025-07-17 17:42:29 +0100

    commit

--- agrilo-new/src/App.jsx
+++ agrilo-new/src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import  './assets/css/all.min.css'
 import NotFound from './pages/NotFound'
 import Home from './pages/Home';
+import TeamMembers  from './pages/teamMembers';
+import Contact from './pages/Contact';
 import ScrollToTop from './components/ScrollToTop';
 
 function Layout() {
@@ -11,6 +14,8 @@ function Layout() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/teamMembers" element={<TeamMembers />} />
         </Routes>
       </div>
     </>
